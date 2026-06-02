ee49971e1a50ef5a7b92f5729134554e7f30e0ee
Marcio Tomazoni <marciotz13@gmail.com>
2026-06-02 05:47:51 -0300

    feat: cria dashboard inicial do controle de ganhos

--- app/src/main/java/com/marciotomazoni/controleganhos/MainActivity.kt
+++ app/src/main/java/com/marciotomazoni/controleganhos/MainActivity.kt
@@ -4,44 +4,82 @@ import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
 import androidx.activity.enableEdgeToEdge
-import androidx.compose.foundation.layout.fillMaxSize
-import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.layout.*
+import androidx.compose.material3.Button
+import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.Scaffold
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
+import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
-import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.ui.unit.dp
 import com.marciotomazoni.controleganhos.ui.theme.ControleGanhosTheme
 
 class MainActivity : ComponentActivity() {
+
     override fun onCreate(savedInstanceState: Bundle?) {
+
         super.onCreate(savedInstanceState)
+
         enableEdgeToEdge()
+
         setContent {
+
             ControleGanhosTheme {
-                Scaffold(modifier = Modifier.fillMaxSize()) { innerPadding ->
-                    Greeting(
-                        name = "Android",
-                        modifier = Modifier.padding(innerPadding)
+
+                Scaffold { paddingValues ->
+
+                    DashboardScreen(
+                        modifier = Modifier.padding(paddingValues)
                     )
+
                 }
+
             }
+
         }
+
     }
+
 }
 
 @Composable
-fun Greeting(name: String, modifier: Modifier = Modifier) {
-    Text(
-        text = "Hello $name!",
+fun DashboardScreen(modifier: Modifier = Modifier) {
+
+    Column(
         modifier = modifier
-    )
-}
+            .fillMaxSize()
+            .padding(24.dp),
+        horizontalAlignment = Alignment.CenterHorizontally
+    ) {
 
-@Preview(showBackground = true)
-@Composable
-fun GreetingPreview() {
-    ControleGanhosTheme {
-        Greeting("Android")
+        Text(
+            text = "🚗 Controle de Ganhos",
+            style = MaterialTheme.typography.headlineMedium
+        )
+
+        Spacer(modifier = Modifier.height(32.dp))
+
+        Text("Ganhos Hoje: R$ 0,00")
+
+        Spacer(modifier = Modifier.height(8.dp))
+
+        Text("Gastos Hoje: R$ 0,00")
+
+        Spacer(modifier = Modifier.height(8.dp))
+
+        Text("Lucro Hoje: R$ 0,00")
+
+        Spacer(modifier = Modifier.height(32.dp))
+
+        Button(
+            onClick = { }
+        ) {
+
+            Text("Nova Corrida")
+
+        }
+
     }
+
 }
